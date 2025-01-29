d7e3aad7471916da8be6b209f49c2a41fde02429
markdormeus <serran.mark.ms@gmail.com>
2025-01-29 01:31:50 -0500

    Created user entity class with sample fields alongside partial CRUD

---
+++ src/main/java/com/mdormeus/springchat/entity/User.java
@@ -0,0 +1,45 @@
+package com.mdormeus.springchat.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+    @Id
+    @Column
+    private int id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
